16369044d399d68029ca5716cc4ab73194b40b27
chobee <chobee@chobee.com>
2022-07-18 16:50:26 +0900

    11111

---
+++ src/Ch08For/C01For.java
@@ -0,0 +1,34 @@
+package Ch08For;
+
+public class C01For {
+
+	public static void main(String[] args) {
+		
+//		int i=1;
+//		while(i<=10)
+//		{
+//			System.out.println("i : " + i);
+//			i++;
+//		}
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println("i : " + i);
+		}
+		
+		//1부터 N까지의 합
+		//N부터 M까지의 합
+		//N부터 M까지의 수중의 3의배수를 제외한 합
+		//구구단 출력
+		//--------중첩for---------------------
+		//전체 구구단 출력
+		//전체 구구단 역순 출력
+		//별찍기(직삼각형,역직각삼각형,정삼각형,역정삼각형...)
+		
+		
+		
+		
+		
+
+	}
+
+}
